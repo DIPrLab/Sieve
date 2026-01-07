91c2d039ef82f7c698b8082e788b1f9d7a536e15
Anadi Shakya <ashakya@pdx.edu>
2026-01-07 11:57:33 -0800

    Adding PostgreSQL wifi dataset deumps (LFS)

---
+++ data/wifi_dataset_pg/wifidata_pg.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5869e9d0f5bb31d6708ff7906eae9069298bc8677c8113659454985548aab3f0
+size 1084922978
---
+++ data/wifi_dataset_pg/wifidefn_pg.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:95ea990312e395a212d3a71e88c38c3af8d6a4fc251c32e6c27cdfbc74be0d51
+size 13983
